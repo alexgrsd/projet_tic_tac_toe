6fb491b413682605d88abb7f400f9423c9333b9f
texidot <alexandre.grosdidier@edu.esiee.fr>
2025-12-05 22:17:26 +0100

    créations de fichiers pour les strings personalisées et reloues a mettre dans le code principal

---
+++ src/custom_strings.cpp
@@ -0,0 +1,13 @@
+#include "custom_strings.hpp"
+
+// déclarations de chaines de strings utilisées pour pas polluer le code principal
+
+std::string game_name_ascii = R"(
+
+ __     __    __     ______     ______     ______   ______     ______   
+/\ \   /\ "-./  \   /\  __ \   /\  ___\   /\__  _\ /\  __ \   /\  ___\  
+\ \ \  \ \ \-./\ \  \ \  __ \  \ \ \____  \/_/\ \/ \ \ \/\ \  \ \  __\  
+ \ \_\  \ \_\ \ \_\  \ \_\ \_\  \ \_____\    \ \_\  \ \_____\  \ \_____\
+  \/_/   \/_/  \/_/   \/_/\/_/   \/_____/     \/_/   \/_____/   \/_____/
+
+)"; // ASCII art du nom du jeu (merci benoit :D)
