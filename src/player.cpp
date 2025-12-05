8099d496ff1fc930f8d0bb6b41c8090ca69ef86f
texidot <alexandre.grosdidier@edu.esiee.fr>
2025-12-05 20:00:16 +0100

    ajout des fonctions au player

--- src/player.cpp
+++ src/player.cpp
@@ -1,1 +1,18 @@
 #include "player.hpp"
+
+namespace player
+{
+
+    Player::Player(char symbol, const char *name) : symbol(symbol), name(name) {}
+
+    char Player::get_symbol() const
+    {
+        return symbol;
+    }
+
+    const char *Player::get_name() const
+    {
+        return name;
+    }
+
+};
