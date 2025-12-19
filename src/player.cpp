ecc9a98c8c80302131349aa9e6460b8581182b5c
texidot <alexandre.grosdidier@edu.esiee.fr>
2025-12-19 12:39:38 +0100

    added diffulties for bot

--- src/player.cpp
+++ src/player.cpp
@@ -1,61 +1,155 @@
 #include "player.hpp"
-#include <string>
+#include "display.hpp"
 
 namespace player
 {
 
     Player::Player(char symbol, const std::string &name, bool is_human) : symbol(symbol), name(name), is_human(is_human) {}
+    Player::Player(char symbol, const std::string &name, bool is_human, int difficulty_level): symbol(symbol), name(name), is_human(is_human), difficulty_level(difficulty_level) {}
 
-    Player create_bot()
+
+    Player create_bot(int &difficulty_level)
     {
-        Player bot('O', "Computer", false);
+        Player bot('O', "Computer", false, difficulty_level);
         return bot;
     }
 
-    void player::Player::play_bot_move(board::Board& board) 
+    int Player::get_difficulty_level() const
     {
-        
-        board::Board current_board = board;
-        auto current_state = current_board.get_grid();
-        // s'il risque de perdre au prochain coup, il bloque l'adversaire
-        char opponent_symbol = (this->get_symbol() == 'X') ? 'O' : 'X';
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                if (current_state[i][j] == ' ') {
-                    current_board.set_cell(i, j, opponent_symbol);
-                    if (current_board.check_winner() == opponent_symbol) {
-                        board.set_cell(i, j, this->get_symbol());
-                        return; // coup joué
-                    }
-                }
-            }
-        }
-        current_board = board; // reset l'état du plateau
-        current_state = current_board.get_grid();
-        // si le bot peut gagner au prochain coup, il le fait
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                if (current_state[i][j] == ' ') {
-                    current_board.set_cell(i, j, this->get_symbol());
-                    if (current_board.check_winner() == this->get_symbol()) {
-                        board.set_cell(i, j, this->get_symbol());
-                        return; // coup joué
-                    }
-                }
-            }
-        }
-        current_board = board; // reset l'état du plateau
-        current_state = current_board.get_grid();
-        // sinon, jouer un coup aléatoire
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                if (current_state[i][j] == ' ') {
-                    board.set_cell(i, j, this->get_symbol());
-                    return; // coup joué
-                }
-            }
-        }
+        return difficulty_level;
+    }
 
+
+
+    void Player::play_bot_move_easy(board::Board &board) // joue une case disponible au hasard
+    {
+        std::srand(static_cast<unsigned int>(std::time(nullptr)));
+        int next_case[2] = {std::rand() % 3, std::rand() % 3};
+        while (true)
+        {
+            if (board.get_cell(next_case[0], next_case[1]) == ' ')
+            {
+                board.set_cell(next_case[0], next_case[1], this->get_symbol());
+                return;
+            }
+            else
+            {
+                next_case[0] = std::rand() % 3;
+                next_case[1] = std::rand() % 3;
+            }
+        }
+    }
+
+        bool detect_victory(board::Board &board, char symbol, int &row, int &col) // dectecte si une victoire est possible au prochain coup pour le symbole donné, et remplit row et col avec la position gagnante
+    {
+        board::Board temp_board = board::Board(board.get_grid());
+        size_t size = board.get_grid().size();
+
+        for (size_t i = 0; i < size; i++)
+        {
+            for (size_t j = 0; j < size; j++)
+            {
+                if (temp_board.get_cell(i, j) == ' ')
+                { // case vide
+                    temp_board.set_cell(i, j, symbol);
+                    if (temp_board.check_winner() == symbol)
+                    {
+                        row = i;
+                        col = j;
+                        return true;
+                    }
+                    else
+                    {
+                        temp_board = board::Board(board.get_grid()); // reset temp_board
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    void Player::play_bot_move_medium(board::Board &board, Player const &human) // bloque si défaite gagne si possible sinon joue totalement au hasard
+    {
+        char opponent_symbol = human.get_symbol();
+        int row{0}, col{0};
+
+        if (detect_victory(board, this->get_symbol(), row, col))
+        {                                                                // si le bot peut gagner ce tour ci le bot gagne
+            board.set_cell(row, col, this->get_symbol());
+            return;
+        }
+        if (detect_victory(board, opponent_symbol, row, col))
+        {                                                                  // si l'adversaire peut gagner ce tour ci le bot bloque
+            board.set_cell(row, col, this->get_symbol());
+            return;
+        }
+        play_bot_move_easy(board);              // si aucune menace, joue aléatoirement en énorme légendre
+    }
+
+    int minimax(board::Board &board, bool is_bot_turn, char bot_symbol, char human_symbol) // retourne le score de la position donnée pour le bot hard
+    {
+        char winner = board.check_winner();
+        if (winner == bot_symbol)
+            return 10;
+        if (winner == human_symbol)
+            return -10;
+        if (board.is_full())
+            return 0;
+
+        int best_score = is_bot_turn ? -1000 : 1000;
+        board::Board temp_board = board::Board(board.get_grid());
+
+        for (size_t i = 0; i < 3; ++i)
+        {
+            for (size_t j = 0; j < 3; ++j)
+            {
+                if (temp_board.get_cell(i, j) == ' ')
+                {
+                    temp_board.set_cell(i, j, is_bot_turn ? bot_symbol : human_symbol);
+
+                    int score = minimax(temp_board, !is_bot_turn, bot_symbol, human_symbol);
+                    temp_board = board::Board(board.get_grid());
+
+                    if (is_bot_turn)
+                        best_score = std::max(best_score, score);
+                    else
+                        best_score = std::min(best_score, score);
+                }
+            }
+        }
+        return best_score;
+    }
+
+    void Player::play_bot_move_hard(board::Board &board, Player const &human)
+    {
+        char bot_symbol = this->get_symbol();
+        char human_symbol = human.get_symbol();
+        board::Board temp_board = board::Board(board.get_grid());
+
+        int best_score = -1000;
+        size_t best_i = 0, best_j = 0;
+
+        for (size_t i = 0; i < 3; ++i)
+        {
+            for (size_t j = 0; j < 3; ++j)
+            {
+                if (temp_board.get_cell(i, j) == ' ')
+                {
+                    temp_board.set_cell(i, j, bot_symbol);
+
+                    int score = minimax(temp_board, false, bot_symbol, human_symbol);
+                    temp_board = board::Board(board.get_grid());
+
+                    if (score > best_score)
+                    {
+                        best_score = score;
+                        best_i = i;
+                        best_j = j;
+                    }
+                }
+            }
+        }
+        board.set_cell(best_i, best_j, bot_symbol);
     }
 
     bool Player::get_is_human() const
--- src/player.hpp
+++ src/player.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <string>
 #include "board.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 namespace player
 {
@@ -11,14 +14,22 @@ namespace player
         char symbol;
         std::string name;
         bool is_human;
+        int difficulty_level;
     public:
         Player(char symbol, const std::string &name, bool is_human);
+        Player(char symbol, const std::string &name, bool is_human, int difficulty_level);
+        int get_difficulty_level() const;
         char get_symbol() const;
         const std::string &get_name() const;
         bool get_is_human() const;
-        void play_bot_move(board::Board& board); 
+        void play_bot_move_easy(board::Board &board);
+        void play_bot_move_medium(board::Board &board, Player const &human); // bloquera juste une victoire imminente de l'adversaire
+        void play_bot_move_hard(board::Board &board, Player const &human); // sera normalement "imbattable" (on y croit)
     };
 
-    Player create_bot();
+    Player create_bot(int &difficulty_level);
 
 } // namespace player
+
+
+
