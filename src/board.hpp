789575408224e46d05b987c8d3d6002389193d83
texidot <alexandre.grosdidier@edu.esiee.fr>
2025-12-19 12:39:13 +0100

    improved board

--- src/board.hpp
+++ src/board.hpp
@@ -6,9 +6,10 @@ namespace board // représente le plateau de jeu
 {
     struct Board{
         private:
-            std::array<std::array<char, 3>, 3> grid;
+            std::array<std::array<char, 3>, 3> grid{};
         public:
             Board();
+            Board(std::array<std::array<char, 3>, 3> grid_init) : grid(grid_init) {}
             char get_cell(int row, int col) const;
             std::array<std::array<char, 3>, 3> get_grid() const;
             bool set_cell(int row, int col, char symbol);
